d5c80a74167520ad63c93bc8997970ae984ede9a
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-02-22 16:47:49 -0800

    moving indoor raining

---
+++ client/src/Router.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route, useParams} from 'react-router-dom'
+// import Sidebar from './components/sidebar/Sidebar'
+
+
+function RouterPage(){
+    
+}
