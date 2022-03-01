5d07a91a95f4702dbfcfd82f5865595c869f1315
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-03-01 15:01:53 -0800

    Come on and work you POS

---
+++ client/src/components/blackboard/Blackboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Blackboard.scss'
+
+function Blackboard() {
+  return (
+    <section className='blackboard'>
+        <h1>Blackboard</h1>
+        <p>Hello</p>
+    </section>
+    
+  )
+}
+
+export default Blackboard
---
+++ client/src/components/home/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import mysrupic from '../../images/mysrupic.png'
+
+import './Header.scss'
+
+function Header() {
+  return (
+    <section className='header'>
+        <img src={mysrupic} alt="" />
+        <div className='campus_announcements_header'>
+            <h2>Campus Announcements</h2>
+        </div>
+        <div className='campus_text'>
+            <b>Disabled Accounts for Non-Continuing Students</b>
+            <p>
+            The University began to de-commission CSUDH Student network and toroMail accounts of students who have not been enrolled in the 
+            campus or graduated for more than two (2) years, have no active application with the University, 
+            and is not a member of the CSUDH Alumni Association. These accounts will be disabled as part of our Identity Management Project.
+            </p>
+
+            
+        </div>
+        
+    </section>
+  )
+}
+
+export default Header
