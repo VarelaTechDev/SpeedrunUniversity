78809f7c2da8518795f20b53b2f502e9c2dd1412
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-04-05 02:12:20 -0700

    Backend refactors/cleaned

--- server/index.js
+++ server/index.js
@@ -1,57 +1,43 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// ? Access to send data to our server with only our weblinks
-const cors = require('cors')
-// ? Allows us access to .env files to protect us when we use secret keys
-const dotenv = require('dotenv').config()
-// ? Gives us a cookie that we can use after we validate the user and keep them logged in
-const cookieParser = require('cookie-parser')
-
 const morgan = require('morgan')
-
 const app = express()
 
-// ? How to remove a specific thing from an array
-// var toRemove = 1;
-// var arr = [1, 2, 3, 4, 5];
-// arr = arr.filter(function(item) {
-//     return item !== toRemove
-// });
-// console.log(arr)
+// ? Access to send data to our server from a WHITELISTED list
+const cors = require('cors')
 
+// ? Gives us the ability to access .env to protect sensitive info
+const dotenv = require('dotenv').config()
 
-// * JSON Middleware + Allow you to POST
+// ? Allows us to access cookies that we can use to validate the user [Keeps them logged in]
+const cookieParser = require('cookie-parser')
+
+// * JSON middleware :: Allow POST 
 app.use(express.json())
-// * A mini-whitelist to what links are and are not allowed to send data
+// * WHITELIST LINKS
 app.use(cors({
     origin:['http://localhost:3000','https://speedrununi.netlify.app'],
     credentials: true,
     exposedHeaders: ['speedrun_cookie']
 }))
 
+// * Gives us mini-details about REQUEST being made to the server
 app.use(morgan('tiny'))
 
-// * Convert cookie strings strings into objects
+// * Cnvert cookie strings --> Objects
 app.use(cookieParser())
+
+app.use('/courses', require('./router/classRouter'))
+app.use('/auth', require('./router/studentRouter'))
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
 
-// * Middleware for sending Tweets
-//app.use('/tweet', require('./router/tweetRouter'))
-app.use('/courses', require('./router/classRouter'))
-
-// * Middleware for logging in
-//app.use('/auth', require('./router/userRouter'))
-app.use('/auth', require('./router/studentRouter'))
-
-// * MongoDB [Local Version for speed]
- 
+// * MongoDB [Local Version for speed] 
 //mongoose.connect(`mongodb+srv://rosenthal:${process.env.MONGO_PASSWORD}@arisadatabase.wez6h.mongodb.net/speedrunDB?retryWrites=true&w=majority`,
-//mongoose.connect(`mongodb+srv://rosenthal:${process.env.MONGO_PASSWORD}@arisadatabase.wez6h.mongodb.net/arisaDatabase?retryWrites=true&w=majority`,
-mongoose.connect(`mongodb://localhost:27017/sru`, 
-    (err) => {
-        if(err) 
-            return console.log(err)
-        console.log('Connected to MongoDB')
-    })
+mongoose.connect(`mongodb://localhost:27017/sru`, (err) => {
+    if(err) return console.log(err)
+    
+    console.log('Connected to MongoDB')
+})
