26ab7f4c694efeb11420cee1966e24c8714e0d64
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-05-05 12:46:33 -0700

    Display course sem two

--- client/src/components/blackboard/courseCatalog/ClassRegister.js
+++ client/src/components/blackboard/courseCatalog/ClassRegister.js
@@ -12,6 +12,8 @@ import ErrorMessage from '../../misc/ErrorMessage'
 
 import './ClassRegister.scss'
 
+import { doAlgo } from "../Main/UserAlgo";
+
 import DisplayModule from "./DisplayTaughtCourses";
 
 function ClassRegister() {
@@ -31,17 +33,28 @@ function ClassRegister() {
     const [semesterOneClasses, setSemesterOneClasses] = useState([])
     const [semesterTwoClasses, setSemesterTwoClasses] = useState([])
     
+    const [reportCardData, setReportCardData] = useState(undefined)
+
     useEffect(() => {
         getClasses()
+        getReportCard()
+        console.log(reportCardData)
     }, [])
 
     function getFirstChar(classId){
         // console.log(classId.charAt(0))
-        console.log(classId.charAt(0) == '1')
+        // console.log(classId.charAt(0) == '1')
         return classId.charAt(0)
     }
 
+    async function getReportCard(){
+        console.log(user.username)
+        const response = await Axios.get(`${domain}/auth/reportCard/${user.username}`)
+        setReportCardData(response.data)
+    }
+
     async function getClasses(){
+        getUser()
         const classResponse = await Axios.get(`${domain}/courses/list`)
         
         const data = classResponse.data
@@ -51,12 +64,17 @@ function ClassRegister() {
         const filterSemesterOne = data.filter((course) => getFirstChar(course.ClassId) == "1")
         setSemesterOneClasses(filterSemesterOne)
         
-        const filterSemesterTwo = data.filter((course) => getFirstChar(course.ClassId) == "2")
-        setSemesterTwoClasses(filterSemesterTwo)
         
         
 
+        const filterSemesterTwo = data.filter((course) => getFirstChar(course.ClassId) == "2")
+        setSemesterTwoClasses(filterSemesterTwo)
+
         setKlasses(classResponse.data)
+    }
+
+    async function recAlgoTheCourses(){
+        return false
     }
 
     return  (
@@ -64,11 +82,10 @@ function ClassRegister() {
             <p>
                 The following classes are taught at Speedrun University
             </p>
-            {klass.length == 0 || user == null? 
+            {klass.length == 0 || user == null || reportCardData == undefined? 
                 ('Loading')    
                 :
-                
-                    user.completedSemesterOne ? 
+                    doAlgo(reportCardData.courses) ? 
                         (
                         semesterTwoClasses.map((item, i) => {
                             return <DisplayModule key={i} Synopsis={item.Synopsis} ClassId={item.ClassId} Name={item.Name} Tag={item.Tag}Professor={item.Professor} UserData={user}/>
@@ -77,6 +94,7 @@ function ClassRegister() {
                         :
                         (
                         semesterOneClasses.map((item, i) => {
+                            
                             return <DisplayModule key={i} Synopsis={item.Synopsis} ClassId={item.ClassId} Name={item.Name} Tag={item.Tag}Professor={item.Professor} UserData={user}/>
                         })
                         )
